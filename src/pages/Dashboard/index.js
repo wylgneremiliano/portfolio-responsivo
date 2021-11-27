ed84bd0e4006d424cdb91cf19ec6d0c9768b9191
wylgneremiliano-7itec <wylgneremiliano@7itec.io>
2021-11-27 02:57:38 -0300

    feat: redirect para youtube

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 import About from "./Components/About";
 import Banner from "./Components/Banner";
 import Header from "./Components/Header";
@@ -7,6 +9,9 @@ import { Container } from "./styles";
 export default function Dashboard() {
   const [goingUp, setGoingUp] = useState(false);
   useEffect(() => {
+    window.location.href =
+      "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley";
+
     onScroll();
   }, [goingUp]);
   const onScroll = () => {
