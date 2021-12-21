d840944b6dfb870fbd029994f246014a85eed169
Wylgner Emiliano <wylgneremiliano@gmail.com>
2021-12-21 07:24:55 -0300

    fix: typo

--- src/pages/Dashboard/Components/Banner/index.js
+++ src/pages/Dashboard/Components/Banner/index.js
@@ -17,7 +17,7 @@ export default function Banner() {
           Oi, eu sou
           <br /> <Span>Wylgner Emiliano.</Span>
         </Title>
-        <SubTitle>Sou desenvolvedor web front-end.</SubTitle>
+        <SubTitle>Desenvolvedor web front-end.</SubTitle>
         <AboutMe>Sobre mim</AboutMe>
       </TextContainer>
     </Container>
